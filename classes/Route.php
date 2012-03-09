d4a3346ccad548fc43f535c1f801751d04e906a5
lex0tanil <adminakos@gmail.com>
2012-03-09 12:51:23 +0000

    Update classes/Route.php

--- classes/Route.php
+++ classes/Route.php
@@ -16,13 +16,13 @@ class Route extends ConnectedItem {
 			$returnObject->error = false;
 			$returnObject->route_id = $data['route_id'];
 			$returnObject->user_id = $data['user_id'];
-            $returnObject->route_title = stripslashes($data['user_facebook_id']);
-            $returnObject->route_nb_points = $data['route_nb_points'];
+            		$returnObject->route_title = stripslashes($data['user_facebook_id']);
+           		$returnObject->route_nb_points = $data['route_nb_points'];
 			$returnObject->route_length = $data['route_length'];
 			$returnObject->route_elevation = $data['route_elevation'];
 			$returnObject->route_elevation_max = $data['route_elevation_max'];
 			$returnObject->route_elevation_min = $data['route_elevation_min'];
-            $returnObject->route_dt = $data['route_dt'];
+          		$returnObject->route_dt = $data['route_dt'];
 		}
 		return $returnObject;
 	}
